4be0ca74c96ce5730073b0bfb4f7e2e7a77184eb
Shuge Wang <shugewang@hotmail.co.uk>
2022-04-23 02:08:55 +0100

    added DOM manipulation

--- myScript.js
+++ myScript.js
@@ -1,25 +1,10 @@
-// function to hide start-container and show game-container
-
-document.getElementById("game-container").style.display = "none";
-document.getElementById("end-container").style.display = "none";
-document.getElementById("startButton").addEventListener("click", function() {
-    document.getElementById("start-container").style.display = "none";
-});
-document.getElementById("startButton").addEventListener("click", function() {
-    document.getElementById("game-container").style.display = "block";
-});
-
-// function to hide game-container and show end-container
-/* once last round
-    document.getElementById("game-container").style.display = "none";
-
-    document.getElementById("end-container").style.display = "block";
-*/
 
 // function for computer to randomly choose an option
-function computerPlay(){
+function getRandomChoice(){
     plays = ["rock", "paper", "scissors"];
-    return randomPlay = plays[Math.floor(Math.random()*plays.length)];
+    randomChoice = plays[Math.floor(Math.random()*plays.length)];
+    document.getElementById("computer-choice").innerHTML = randomChoice;
+    return randomChoice;
 }
 
 // function to capitalize a word
@@ -54,32 +39,75 @@ function playRound(playerSelection, computerSelection){
     console.log("Invalid input. Try again!");
 }
 
-// main function: play 5 rounds with user input
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock, paper, or scissors?");
-        let computerSelection = computerPlay();
+// function to hide start-container and show game-container
+document.getElementById("game-container").style.display = "none";
+document.getElementById("end-container").style.display = "none";
+document.getElementById("startButton").addEventListener("click", function() {
+    document.getElementById("start-container").style.display = "none";
+});
+document.getElementById("startButton").addEventListener("click", function() {
+    document.getElementById("game-container").style.display = "block";
+});
 
-        winner = playRound(playerSelection, computerSelection);
-        if (winner == "player"){
-            playerScore += 1;
-            console.log( "You win! " + capitalize(playerSelection) + " beats " + capitalize(computerSelection) + "!");
-        } else if (winner == "computer"){
-            computerScore += 1;
-            console.log("You lose! " + capitalize(computerSelection) + " beats " + capitalize(playerSelection) + "!");
+function finishGame (){
+    let champion = "";
+        //hide 
+        document.getElementById("game-container").style.display = "none";
+        document.getElementById("end-container").style.display = "block";
+        if (computerScore > playerScore) {
+            champion = "The Machine!";
         } else {
-            console.log("It's a draw!");
+            champion = "You!";
         }
-        console.log("Your score: " + playerScore);
-        console.log("Computer score:" + computerScore);
-    }
-    if (playerScore > computerScore) { 
-        console.log("Player is the winner!");
-    } else if (computerScore > playerScore) {
-        console.log("Computer is the winner!");
+        document.getElementById("winner").innerHTML = champion;
+}
+
+// set global variables
+let playerScore = 0;
+let computerScore = 0;
+let anyScore = 0;
+// main game function
+function game() {
+    document.getElementById("rockBtn").addEventListener("click", function () {
+        updateScore("rock");
+    })
+    document.getElementById("paperBtn").addEventListener("click", function () {
+        updateScore("paper");
+    })
+    document.getElementById("scissorsBtn").addEventListener("click", function () {
+        updateScore("scissors");
+    })
+    
+}
+
+// play a round and update score
+function updateScore(playerSelection){
+    let computerSelection = getRandomChoice();
+    let winner = playRound(playerSelection, computerSelection);
+    let result = "";
+    if (winner == "player"){
+        playerScore += 1;
+        result = "You win! " + capitalize(playerSelection) + " beats " + capitalize(computerSelection) + "!";
+    } else if (winner == "computer"){
+        computerScore += 1;
+        result = "You lose! " + capitalize(computerSelection) + " beats " + capitalize(playerSelection) + "!";
     } else {
-        console.log("It's a draw!");
+        result = "It's a draw!";
+    }
+    console.log("Your score: " + playerScore);
+    console.log("Computer score:" + computerScore);
+    console.log(result);
+
+    let update = document.createElement("p");
+    let node = document.createTextNode(result);
+    update.appendChild(node);
+    document.getElementById("updates").appendChild(update);
+
+    document.getElementById("player-score").innerHTML = playerScore;
+    document.getElementById("computer-score").innerHTML = computerScore;
+    
+    if (computerScore == 5 || playerScore == 5){
+        finishGame();
     }
 }
+game();
